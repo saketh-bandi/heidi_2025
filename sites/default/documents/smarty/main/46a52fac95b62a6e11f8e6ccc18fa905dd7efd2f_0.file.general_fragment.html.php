c53e7bfceb5ce9619e9add8bb1a316de3f445fb3
nihalmaddala <nihalmaddala@gmail.com>
2025-11-22 17:56:34 -0800

    Revert all Heidi Health API integration changes
    - Deleted 14 Heidi-specific files (test files, SQL schema, service class, interface fragments)
    - Reverted demographics.php (removed 615+ lines of Heidi JavaScript integration)
    - Reverted vitals_fragment.php and pnuserapi.php to original state
    - Removed Heidi API configuration from .env.example
    - Preserved python_agent folder completely intact

---
+++ sites/default/documents/smarty/main/46a52fac95b62a6e11f8e6ccc18fa905dd7efd2f_0.file.general_fragment.html.php
@@ -0,0 +1,77 @@
+<?php
+/* Smarty version 4.5.6, created on 2025-11-22 21:04:51
+  from '/Users/nihalmaddala/heidi_2025/templates/prescription/general_fragment.html' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '4.5.6',
+  'unifunc' => 'content_69222573e44cb5_79711764',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '46a52fac95b62a6e11f8e6ccc18fa905dd7efd2f' => 
+    array (
+      0 => '/Users/nihalmaddala/heidi_2025/templates/prescription/general_fragment.html',
+      1 => 1763844353,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_69222573e44cb5_79711764 (Smarty_Internal_Template $_smarty_tpl) {
+$_smarty_tpl->_checkPlugins(array(0=>array('file'=>'/Users/nihalmaddala/heidi_2025/library/smarty/plugins/function.xlt.php','function'=>'smarty_function_xlt',),));
+if (empty($_smarty_tpl->tpl_vars['prescriptions']->value)) {
+echo smarty_function_xlt(array('t'=>'None'),$_smarty_tpl);?>
+
+<?php } else { ?>
+<div class="table-responsive">
+    <table class="table table-sm table-striped">
+        <thead>
+            <tr>
+                <th><?php echo smarty_function_xlt(array('t'=>'Drug'),$_smarty_tpl);?>
+</th>
+                <th><?php echo smarty_function_xlt(array('t'=>'Details'),$_smarty_tpl);?>
+</th>
+                <th><?php echo smarty_function_xlt(array('t'=>'Qty'),$_smarty_tpl);?>
+</th>
+                <th><?php echo smarty_function_xlt(array('t'=>'Refills'),$_smarty_tpl);?>
+</th>
+                <th><?php echo smarty_function_xlt(array('t'=>'Filled'),$_smarty_tpl);?>
+</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+$_from = $_smarty_tpl->smarty->ext->_foreach->init($_smarty_tpl, $_smarty_tpl->tpl_vars['prescriptions']->value, 'prescription');
+$_smarty_tpl->tpl_vars['prescription']->do_else = true;
+if ($_from !== null) foreach ($_from as $_smarty_tpl->tpl_vars['prescription']->value) {
+$_smarty_tpl->tpl_vars['prescription']->do_else = false;
+?>
+            <?php if ($_smarty_tpl->tpl_vars['prescription']->value->get_active() > 0) {?>
+            <tr>
+                <td><?php echo text($_smarty_tpl->tpl_vars['prescription']->value->drug);?>
+&nbsp;</td>
+                <td><?php echo text($_smarty_tpl->tpl_vars['prescription']->value->get_size());
+echo text($_smarty_tpl->tpl_vars['prescription']->value->get_unit_display());?>
+&nbsp;
+                    <?php echo text($_smarty_tpl->tpl_vars['prescription']->value->get_dosage_display());?>
+</td>
+                <td><?php echo text($_smarty_tpl->tpl_vars['prescription']->value->get_quantity());?>
+</td>
+                <td><?php echo text($_smarty_tpl->tpl_vars['prescription']->value->get_refills());?>
+</td>
+                <td><?php echo text($_smarty_tpl->tpl_vars['prescription']->value->get_date_added());?>
+</td>
+            </tr>
+            <?php }?>
+            <?php
+}
+$_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl, 1);?>
+        </tbody>
+    </table>
+</div>
+<?php }
+}
+}
